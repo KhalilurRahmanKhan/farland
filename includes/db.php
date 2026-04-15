ba5e047fecb0c28f675bbcb01384782f4e4b69d8
Khalilur Rahman Khan <khanjahid373@gmail.com>
2026-04-15 18:06:13 +0800

    project setup

---
+++ includes/db.php
@@ -0,0 +1,7 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "farland");
+
+if (!$conn) {
+    die("DB connection failed");
+}
+?>
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include "includes/db.php"; ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>My Project</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<h1>Hello, My Website 🚀</h1>
+
+<script src="js/script.js"></script>
+</body>
+</html>
