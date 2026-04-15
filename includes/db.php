087616ab828408a62cf437a54eb7b40d4929e047
Khalilur Rahman Khan <khanjahid373@gmail.com>
2026-04-15 21:17:52 +0800

    use external config

--- includes/db.php
+++ includes/db.php
@@ -1,17 +1,10 @@
 
 <?php
-
-$host = $_SERVER['HTTP_HOST'];
-
-if ($host == 'localhost' || $host == '127.0.0.1') {
-    require "config.local.php";
-} else {
-    require "config.live.php";
-}
+require __DIR__ . '/../config.live.php';
 
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 if (!$conn) {
-    die("Database connection failed");
+    die("DB connection failed: " . mysqli_connect_error());
 }
 ?>
