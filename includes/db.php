5212a5126183403fd11c535e68d3117fc9491797
Khalilur Rahman Khan <khanjahid373@gmail.com>
2026-04-15 20:55:31 +0800

    db updated

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,17 @@
 
 <?php
-$conn = mysqli_connect("localhost", "farland", "zzp4qY@@DWnuZC7", "farland");
+
+$host = $_SERVER['HTTP_HOST'];
+
+if ($host == 'localhost' || $host == '127.0.0.1') {
+    require "config.local.php";
+} else {
+    require "config.live.php";
+}
+
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 if (!$conn) {
-    die("DB connection failed");
+    die("Database connection failed");
 }
 ?>
