d4cb14bd69d30fd7dc4c0afa708e8977ad58dd53
Khalilur Rahman Khan <khanjahid373@gmail.com>
2026-04-16 13:55:54 +0800

    home carousel added

--- js/script.js
+++ js/script.js
@@ -36,6 +36,44 @@ window.addEventListener("scroll", () => {
 
 
 
+document.addEventListener("DOMContentLoaded", function () {
+
+  const slides = document.querySelectorAll(".slide");
+  const nextBtn = document.querySelector(".next");
+  const prevBtn = document.querySelector(".prev");
+
+  if (!slides.length) return; // safety
+
+  let current = 0;
+
+  function showSlide(index) {
+    slides.forEach(slide => slide.classList.remove("active"));
+    slides[index].classList.add("active");
+  }
+
+  function nextSlide() {
+    current = (current + 1) % slides.length;
+    showSlide(current);
+  }
+
+  function prevSlide() {
+    current = (current - 1 + slides.length) % slides.length;
+    showSlide(current);
+  }
+
+  /* BUTTONS SAFE CHECK */
+  if (nextBtn) nextBtn.addEventListener("click", nextSlide);
+  if (prevBtn) prevBtn.addEventListener("click", prevSlide);
+
+  /* AUTO SLIDE */
+  setInterval(nextSlide, 5000);
+
+});
+
+
+
+
+
 
 
 function closeModal() {
